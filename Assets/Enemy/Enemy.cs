7eb6839b6a87aea36e9c7e19500ac23531daae2f
Jonny Farley <jonny.farley@sony.com>
2019-01-24 13:18:56 +0000

    Create Enemy type
    Create Enemy prefab, with attack radius so that he follows the player
    when in range.
    Navmesh baked onto terrain.

---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float attackRadius = 5f;
+
+    private GameObject player = null;
+    private AICharacterControl ai = null;
+
+    // Start is called before the first frame update
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        ai = GetComponentInChildren<AICharacterControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) <= attackRadius) {
+            ai.SetTarget(player.transform);
+        }
+        else {
+            ai.SetTarget(transform);
+        }
+    }
+
+    private void LateUpdate() {
+        transform.position += ai.transform.localPosition; ;
+        ai.transform.localPosition = Vector3.zero;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+    }
+}
