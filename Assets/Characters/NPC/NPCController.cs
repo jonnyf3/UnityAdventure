91188d6e6f98f89dbdd349951d54277826dd5eea
jfarley <jfarley3@hotmail.co.uk>
2019-02-01 23:56:29 +0000

    NPC improvements
    Create special animator controller and script for NPC, which sits idle when player
    is absent and stands up when they approach, and falls down after being
    attacked.

---
+++ Assets/Characters/NPC/NPCController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using RPG.Core;
+
+namespace RPG.Characters
+{
+    public class NPCController : MonoBehaviour, IDamageable
+    {
+        [SerializeField] float activationRadius = 6f;
+        private Animator animator = null;
+
+        // Start is called before the first frame update
+        void Start() {
+            animator = GetComponentInChildren<Animator>();
+
+            var sphereCollider = gameObject.AddComponent<SphereCollider>();
+            sphereCollider.isTrigger = true;
+            sphereCollider.radius = activationRadius;
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.gameObject.GetComponent<Player>()) {
+                animator.SetBool("PlayerInRange", true);
+            }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if (other.gameObject.GetComponent<Player>()) {
+                animator.SetBool("PlayerInRange", false);
+            }
+        }
+
+        public void TakeDamage(float damage) {
+            //NPCs do not actually take damage, but should still respond to being attacked
+            animator.SetTrigger("Attacked");
+        }
+    }
+}
