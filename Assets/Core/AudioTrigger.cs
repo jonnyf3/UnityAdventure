495781accf5bfb69daf96c07ef41b815e984a676
Jonny Farley <jonny.farley@sony.com>
2019-02-01 11:54:45 +0000

    Create AudioTrigger to play audio clips

---
+++ Assets/Core/AudioTrigger.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using RPG.Characters;
+
+namespace RPG.Core
+{
+    public class AudioTrigger : MonoBehaviour
+    {
+        [SerializeField] AudioClip clip;
+        [SerializeField] float triggerRadius = 5f;
+        [SerializeField] bool isOneTimeOnly = true;
+        private bool hasPlayed = false;
+
+        private AudioSource source;
+    
+        void Start() {
+            //Add required components via code at runtime
+            source = gameObject.AddComponent<AudioSource>();
+            source.playOnAwake = false;
+            source.clip = clip;
+
+            var triggerVolume = gameObject.AddComponent<SphereCollider>();
+            triggerVolume.radius = triggerRadius;
+            triggerVolume.isTrigger = true;
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.GetComponent<Player>()) {
+                PlayAudioClip();
+            }
+        }
+
+        private void PlayAudioClip() {
+            if (isOneTimeOnly && hasPlayed) { return; }
+
+            if (!source.isPlaying) {
+                source.Play();
+                hasPlayed = true;
+            }
+        }
+
+        //Visualise trigger area, even though collider is only added at run-time
+        private void OnDrawGizmos() {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, triggerRadius);
+        }
+    }
+}
