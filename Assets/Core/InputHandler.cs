9d6f201e9f4f5bea53d98e6907bdfdd849c5ffa2
Jonny Farley <jonny.farley@sony.com>
2019-02-06 12:25:54 +0000

    Improvements to D-PAD input handling
    Wrap D-pad input handling in methods to treat it like discrete buttons
    rather than axes, so equips will not be cycled multiple times per
    second

--- Assets/Core/InputHandler.cs
+++ Assets/Core/InputHandler.cs
@@ -9,6 +9,9 @@ namespace RPG.Core
         private Player player;
         private bool isPlayerDead = false;
 
+        float lastFrameDPADhorizontal = 0;
+        float lastFrameDPADvertical = 0;
+
         private void Start() {
             player = GameObject.FindObjectOfType<Player>();
             player.onPlayerDied += OnPlayerDied;
@@ -17,10 +20,7 @@ namespace RPG.Core
         // Process any input
         void FixedUpdate() {
             if (isPlayerDead) {
-                if (Input.GetButtonDown("X")) {
-                    player.Respawn();
-                    isPlayerDead = false;
-                }
+                PlayerDeathUpdate();
                 return;
             }
 
@@ -38,23 +38,74 @@ namespace RPG.Core
                 player.MagicAttack();
             }
 
-            //TODO wrap into button-style methods, since this can fire multiple times per second
-            if (Input.GetAxis("DpadVertical") > 0.75f) {
-                player.gameObject.GetComponent<PlayerCombat>().CycleWeapon(1);
+            if (GetVerticalButtonsDown()) {
+                var verticalButtonDirection = Math.Sign(lastFrameDPADvertical);
+                player.gameObject.GetComponent<PlayerCombat>().CycleWeapon(verticalButtonDirection);
             }
-            if (Input.GetAxis("DpadVertical") < -0.75f) {
-                player.gameObject.GetComponent<PlayerCombat>().CycleWeapon(-1);
+            if (GetHorizontalButtonsDown()) {
+                var horizontalButtonDirection = Math.Sign(lastFrameDPADhorizontal);
+                player.gameObject.GetComponent<PlayerCombat>().CycleMagic(horizontalButtonDirection);
             }
-            if (Input.GetAxis("DpadHorizontal") > 0.75f) {
-                player.gameObject.GetComponent<PlayerCombat>().CycleMagic(1);
+        }
+
+        //Wrapper methods to make DPAD axes behave like discrete buttons
+        private bool GetHorizontalButtonsDown() {
+            var thisFrameDPADhorizontal = Input.GetAxis("DpadHorizontal");
+
+            //Button not pressed this frame
+            if (thisFrameDPADhorizontal == 0) {
+                lastFrameDPADhorizontal = thisFrameDPADhorizontal;
+                return false;
             }
-            if (Input.GetAxis("DpadHorizontal") < -0.75f) {
-                player.gameObject.GetComponent<PlayerCombat>().CycleMagic(-1);
+            //Button press same as this frame
+            if (thisFrameDPADhorizontal == lastFrameDPADhorizontal) {
+                return false;
             }
+            //Button press same direction as this frame (but different size)
+            if ((thisFrameDPADhorizontal > 0 && lastFrameDPADhorizontal > 0) ||
+                (thisFrameDPADhorizontal < 0 && lastFrameDPADhorizontal < 0))
+            {
+                lastFrameDPADhorizontal = thisFrameDPADhorizontal;
+                return false;
+            }
+
+            //Any other case should return true
+            lastFrameDPADhorizontal = thisFrameDPADhorizontal;
+            return true;
+        }
+        private bool GetVerticalButtonsDown() {
+            var thisFrameDPADvertical = Input.GetAxis("DpadVertical");
+
+            //Button not pressed this frame
+            if (thisFrameDPADvertical == 0) {
+                lastFrameDPADvertical = thisFrameDPADvertical;
+                return false;
+            }
+            //Button press same as this frame
+            if (thisFrameDPADvertical == lastFrameDPADvertical) {
+                return false;
+            }
+            //Button press same direction as this frame (but different size)
+            if ((thisFrameDPADvertical > 0 && lastFrameDPADvertical > 0) ||
+                (thisFrameDPADvertical < 0 && lastFrameDPADvertical < 0))
+            {
+                lastFrameDPADvertical = thisFrameDPADvertical;
+                return false;
+            }
+
+            //Any other case should return true
+            lastFrameDPADvertical = thisFrameDPADvertical;
+            return true;
         }
 
         private void OnPlayerDied() {
             isPlayerDead = true;
+        }
+        private void PlayerDeathUpdate() {
+            if (Input.GetButtonDown("X")) {
+                player.Respawn();
+                isPlayerDead = false;
+            }
         }
     }
 }
