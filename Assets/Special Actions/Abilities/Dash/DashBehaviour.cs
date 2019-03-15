beba1665bd1071ad1e3bc7bf9e959eb3622514f8
jfarley <jfarley3@hotmail.co.uk>
2019-03-15 09:48:44 +0000

    Improve dash mechanics
    Make dashing more precise, so the character doesn't overshoot the
    destination.  Also use the viewpoint when focussing, to choose a
    destination more accurately, and to allow dashing up to elevated areas
    when the viewer target is a solid object/wall

--- Assets/Special Actions/Abilities/Dash/DashBehaviour.cs
+++ Assets/Special Actions/Abilities/Dash/DashBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 using RPG.Control;
+using RPG.UI;
 
 namespace RPG.Actions
 {
@@ -12,34 +13,34 @@ namespace RPG.Actions
         private Material GhostMaterial => (Data as DashData).dashMaterial;
         private Material GhostBadMaterial => (Data as DashData).badMaterial;
 
+        private Vector3 dashTarget;
+        Vector3 raycastOffset = (0.25f * Vector3.up);
+        
         public override void Use() {
             StartCoroutine(Dash());
         }
 
         private IEnumerator Dash() {
-            Vector3 moveTarget = Vector3.zero;
-            Vector3 offset = (0.25f * Vector3.up);
-
             GameObject model = Instantiate(GhostPrefab);
-            while (Input.GetButton(ControllerInput.ABILITY_BUTTON)) {
-                var direction = GetDirection(Input.GetAxis(ControllerInput.MOVE_Y_AXIS),
-                                             Input.GetAxis(ControllerInput.MOVE_X_AXIS));
-                moveTarget = GetGroundPosition(transform.position + offset + direction * Range);
 
-                if (moveTarget != Vector3.zero) {
-                    model.transform.position = moveTarget;
+            Vector3 lookTarget = GetLookTarget();
+            while (Input.GetButton(ControllerInput.ABILITY_BUTTON)) {
+                lookTarget = GetLookTarget();
+
+                if (GetValidDashDestination(lookTarget - transform.position)) {
+                    model.transform.position = dashTarget;
                     model.GetComponent<MeshRenderer>().material = GhostMaterial;
                 } else {
-                    model.transform.position = transform.position + direction * Range;
+                    model.transform.position = lookTarget;
                     model.GetComponent<MeshRenderer>().material = GhostBadMaterial;
                 }
-                model.transform.forward = direction;
+                model.transform.forward = transform.forward;
                 yield return new WaitForEndOfFrame();
             }
 
             Destroy(model);
-            if (moveTarget != Vector3.zero) {
-                StartCoroutine(DashToTarget(moveTarget));
+            if (GetValidDashDestination(lookTarget - transform.position)) {
+                StartCoroutine(DashToTarget(dashTarget));
                 AbilityUsed();
             }
         }
@@ -53,6 +54,7 @@ namespace RPG.Actions
             rigidBody.detectCollisions = false;
             rigidBody.useGravity = false;
 
+            var animStartTime = Time.time;
             var startPosition = transform.position;
             var vecToTarget = position - transform.position;
             var travelDistance = vecToTarget.magnitude;
@@ -60,59 +62,80 @@ namespace RPG.Actions
                 transform.position += vecToTarget.normalized * Speed * Time.deltaTime;
                 yield return new WaitForEndOfFrame();
             }
+            //prevent moving beyond target because of animator root motion
+            do {
+                transform.position = position;
+                rigidBody.velocity = Vector3.zero;
+                yield return new WaitForEndOfFrame();
+            } while (Time.time - animStartTime <= Data.AnimClip.length);
 
-            transform.position = position;
             renderer.material = startMaterial;
             rigidBody.detectCollisions = true;
             rigidBody.useGravity = true;
         }
 
-        private Vector3 GetDirection(float forward, float right) {
-            if (forward == 0 && right == 0) { return transform.forward; }
-
-            var camera = GetComponent<CameraController>();
-            return (forward * camera.Forward + right * camera.Right).normalized;
+        private Vector3 GetLookTarget() {
+            if (Input.GetButton(ControllerInput.FOCUS_BUTTON)) {
+                var viewerTarget = FindObjectOfType<Viewer>().LookTarget;
+                if (Vector3.Distance(transform.position, viewerTarget) > Range) {
+                    var lookVector = (viewerTarget - transform.position).normalized;
+                    viewerTarget = transform.position + (lookVector.normalized * Range);
+                }
+                return viewerTarget;
+            } else {
+                return transform.position + raycastOffset + (transform.forward * Range);
+            }
         }
 
-        private Vector3 GetGroundPosition(Vector3 target) {
-            Vector3 offset = (Vector3.up * 0.25f);  //offset to avoid raycasting at ground level
-            if (Physics.Raycast(transform.position + offset, target - transform.position, out RaycastHit hitInfo, Range, ~0, QueryTriggerInteraction.Ignore)) {
+        private bool GetValidDashDestination(Vector3 direction) {
+            Vector3 target = transform.position + (direction * Range);
+            if (Physics.Raycast(transform.position + raycastOffset, direction, out RaycastHit hitInfo, Range, ~0, QueryTriggerInteraction.Ignore)) {
+                //Dash direction is obstructed
                 var obstruction = hitInfo.transform.gameObject;
                 if (obstruction.isStatic) {
                     if (obstruction == Terrain.activeTerrain.gameObject) {
-                        target = hitInfo.point + offset;  //offset so that ground-finding raycast will hit ground
+                        target = hitInfo.point + raycastOffset;  //offset so that ground-finding raycast will hit ground
                     } else {
-                        //set target just short of any other static obstruction
-                        var vecToTarget = hitInfo.point - transform.position;
-                        var reducedVecToTarget = vecToTarget - (2 * GetComponent<CapsuleCollider>().radius * vecToTarget.normalized);
-                        
-                        target = transform.position + reducedVecToTarget;
+                        if (FindGroundAbove(hitInfo.point + (direction.normalized * 0.2f))) {
+                            return true;
+                        } else {
+                            //set target just short of any other static obstruction
+                            target = hitInfo.point - (2 * GetComponent<CapsuleCollider>().radius)* direction.normalized;
+                        }
                     }
+
+                    if (FindGroundAbove(target) || FindGroundBelow(target)) { return true; }
                 }
             }
 
-            return FindGroundAboveOrBelow(target);
+            //no obstruction; check back along direction for a point where FindGroundBelow is in range
+            while (!FindGroundBelow(target) && Vector3.Distance(target, transform.position) > 0.5f) {
+                target -= direction * 0.2f;
+            }
+            return FindGroundBelow(target);
         }
 
-        private Vector3 FindGroundAboveOrBelow(Vector3 point) {
-            float maxUpAngle = 20f;
-            float maxDownAngle = 45f;
-            float maxUpDistance   = (transform.position - point).magnitude * Mathf.Tan(maxUpAngle * Mathf.Deg2Rad);
-            float maxDownDistance = (transform.position - point).magnitude * Mathf.Tan(maxDownAngle * Mathf.Deg2Rad);
+        private bool FindGroundAbove(Vector3 point) {
+            float maxUpDistance = Mathf.Sqrt(Range*Range - (transform.position - point).sqrMagnitude);
 
-            //find ground below target
+            if (Physics.Raycast(point + (Vector3.up * maxUpDistance), Vector3.down, out RaycastHit hitInfo,
+                                maxUpDistance, ~0, QueryTriggerInteraction.Ignore)) {
+                dashTarget = hitInfo.point;
+                return true;
+            }
+            return false;
+        }
+
+        private bool FindGroundBelow(Vector3 point) {
+            float maxDownDistance = Mathf.Sqrt(Range * Range - (transform.position - point).sqrMagnitude);
+            
+            Debug.DrawLine(point, point + Vector3.down * maxDownDistance, Color.red);
             if (Physics.Raycast(point, Vector3.down, out RaycastHit hitInfo,
                                 maxDownDistance, ~0, QueryTriggerInteraction.Ignore)) {
-                return hitInfo.point;
+                dashTarget = hitInfo.point;
+                return true;
             }
-            //find ground above target
-            else if (Physics.Raycast(point + (Vector3.up * maxUpDistance), Vector3.down, out hitInfo,
-                                     maxUpDistance, ~0, QueryTriggerInteraction.Ignore)) {
-                return hitInfo.point;
-            }
-            else {
-                return Vector3.zero;
-            }
+            return false;
         }
     }
 }
