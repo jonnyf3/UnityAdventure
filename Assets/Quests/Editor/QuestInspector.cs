e0dc8fa2287eb39529c76cf98da2128b503110f0
Jonny Farley <jonny.farley@sony.com>
2019-03-25 15:15:25 +0000

    Add custom inspector for Quest assets
    Quests contain a lot of public lists, which they need in order to be
    able to serialize/save information about their objectives/dependencies.
    This makes the inspector for quests messy, so a custom inspector
    overrides displaying all these lists and only shows the essential quest
    data (e.g. name, experience points)

---
+++ Assets/Quests/Editor/QuestInspector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+namespace RPG.Quests
+{
+    [CustomEditor(typeof(Quest))]
+    public class QuestInspector : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI() {
+            Quest quest = (Quest)target;
+
+            quest.questName = EditorGUILayout.TextField("Quest Name", quest.questName);
+            quest.experiencePoints = EditorGUILayout.IntField("Experience", quest.experiencePoints);
+
+            //TODO button to open quest editor window?
+        }
+    }
+}
