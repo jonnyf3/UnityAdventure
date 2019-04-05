b58964bc6190ab417f82f8f242f79daa3e1e869d
Jonny Farley <jonny.farley@sony.com>
2019-04-05 14:43:49 +0100

    Create simple portal for moving between scenes
    Create portal gameobject with trigger volume, which loads up a specified
    scene asynchronously and then changes scene once the next scene is ready

---
+++ Assets/SceneManagement/Portal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using RPG.Characters;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.GetComponent<Player>()) { StartCoroutine(ChangeScene()); }
+        }
+
+        private IEnumerator ChangeScene() {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            print("Scene finished loading!");
+            Destroy(gameObject);
+        }
+    }
+}
