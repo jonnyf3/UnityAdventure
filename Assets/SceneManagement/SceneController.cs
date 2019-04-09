ddaf922cfabc5a7a7ded5803ba48e414a01c64e8
Jonny Farley <jonny.farley@sony.com>
2019-04-09 13:35:02 +0100

    Create _Main master scene
    Create a "manager" scene, which should always be loaded and which other
    scenes should be loaded to additively.
    Create links from main menu to first level via a button

---
+++ Assets/SceneManagement/SceneController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class SceneController : MonoBehaviour
+    {
+        private static SceneController instance;
+        private static string currentScene;
+        private void Awake() { instance = this; }
+
+        private const string MAIN_MENU = "MainMenu";
+        public const string SCENE_1 = "Level";
+        public const string SCENE_2 = "Dash Level";
+        public const string SANDBOX = "Sandbox";
+
+        void Start() {
+            LoadLevel(MAIN_MENU);
+        }
+
+        public static void LoadLevel(string sceneName) {
+            instance.StartCoroutine(instance.LoadScene(sceneName));
+        }
+
+        private IEnumerator LoadScene(string sceneName) {
+            yield return Load(sceneName);
+            var newScene = SceneManager.GetSceneByName(sceneName);
+            SceneManager.SetActiveScene(newScene);
+
+            yield return UnloadCurrent();
+            currentScene = sceneName;
+        }
+
+        private AsyncOperation Load(string sceneName) {
+            if (SceneManager.GetSceneByName(sceneName).isLoaded) { return null; }
+
+            return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        }
+        private AsyncOperation UnloadCurrent() {
+            if (currentScene == null) { return null; }
+
+            return SceneManager.UnloadSceneAsync(currentScene);
+        }
+    }
+}
---
+++ Assets/UI/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using RPG.SceneManagement;
+
+namespace RPG.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void NewGame() {
+            SceneController.LoadLevel(SceneController.SCENE_1);
+        }
+    }
+}
