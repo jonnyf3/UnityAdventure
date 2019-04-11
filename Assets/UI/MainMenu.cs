0684442e9d57348bbf4500d3b1d7dcd5e5375482
Jonny Farley <jonny.farley@sony.com>
2019-04-11 12:42:10 +0100

    Add arrow indicator to main menu
    Add an arrow icon to identify the currently selected menu option, which
    can be moved appropriately with the DPAD buttons

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -12,28 +12,36 @@ namespace RPG.UI
 
         private List<Button> buttons;
         private Button activeButton;
+        [SerializeField] Image activeButtonIcon = null;
 
         private void Start() {
             buttons = new List<Button>(GetComponentsInChildren<Button>());
             activeButton = buttons[0];
+            MovePointer();
         }
 
         private void Update() {
             if (ControllerInput.GetVerticalButtonsDown(out int direction)) {
-                var newIndex = (buttons.IndexOf(activeButton) + direction) % buttons.Count;
+                var newIndex = (buttons.IndexOf(activeButton) - direction) % buttons.Count;
                 activeButton = buttons[newIndex];
+                MovePointer();
             }
 
             if (Input.GetButtonDown(ENTER_BUTTON)) { activeButton.onClick.Invoke(); }
+        }
+        private void MovePointer() {
+            activeButtonIcon.transform.SetParent(activeButton.transform);
+            activeButtonIcon.transform.localPosition = new Vector2(-70f, 0f);
         }
 
         public void NewGame() {
             FindObjectOfType<SceneController>().LoadLevel(SceneController.SCENE_1);
         }
         public void LoadGame() {
-            //TODO
+            print("Load game");
         }
         public void Quit() {
+            print("Quit game");
             Application.Quit();
         }
     }
