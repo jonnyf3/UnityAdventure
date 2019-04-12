0029aacc9f146d95b536b327a144ca14c575fcd7
Jonny Farley <jonny.farley@sony.com>
2019-04-12 17:21:30 +0100

    Add basic SaveManager skeleton
    Create a SaveManager in the persistent _Main scene with Save() and
    Load() functions

---
+++ Assets/Saving/SaveManager.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SaveManager : MonoBehaviour
+    {
+        private string saveFile = "save";
+        private string SaveFile {
+            get { return Path.Combine(Application.persistentDataPath, saveFile + ".sav"); }
+        }
+
+        public void Save() {
+            print("Saving game to " + SaveFile);
+        }
+
+        public void Load() {
+            print("Loading game from " + SaveFile);
+        }
+    }
+}
