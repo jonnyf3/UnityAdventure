3569cc6b492f728e5d230319d480422b65eb0719
Jonny Farley <jonny.farley@sony.com>
2019-04-16 13:32:30 +0100

    Implement GUIDs for SaveableEntity
    Objects which should be saveable now have a permanent GUID associated to
    them, which will be used to save their state appropriately into the
    global save state dictionary

---
+++ Assets/Saving/Editor/SaveableEntityInspector.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace RPG.Saving
+{
+    [CustomEditor(typeof(SaveableEntity))]
+    public class SaveableEntityInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var se = (SaveableEntity)target;
+            EditorGUILayout.LabelField("GUID:", se.GUID);
+        }
+    }
+}
--- Assets/Saving/SaveableEntity.cs
+++ Assets/Saving/SaveableEntity.cs
@@ -1,20 +1,35 @@
 ﻿using UnityEngine;
+using UnityEditor;
 
 namespace RPG.Saving
 {
-    class SaveableEntity : MonoBehaviour
+    [ExecuteAlways]
+    public class SaveableEntity : MonoBehaviour
     {
+        [SerializeField] string guid = "";
         public string GUID {
-            get { return ""; }
+            get { return guid; }
         }
 
         public object SaveState() {
             print("Saving state for " + gameObject.name + " (" + GUID + ")");
             return null;
         }
-
         public void LoadState(object state) {
             print("Loading state for " + gameObject.name + " (" + GUID + ")");
+        }
+
+        private void Update() {
+            bool isPrefab = string.IsNullOrEmpty(gameObject.scene.path);
+            if (Application.IsPlaying(gameObject) || isPrefab) { return; }
+
+            var so = new SerializedObject(this);
+            var guidProperty = so.FindProperty("guid");
+
+            if (string.IsNullOrEmpty(guidProperty.stringValue)) {
+                guidProperty.stringValue = System.Guid.NewGuid().ToString();
+                so.ApplyModifiedProperties();
+            }
         }
     }
 }
